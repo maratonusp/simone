6019808b9cbe1bcc02fb0aac0b974bdb078362e5
Pedro Sousa <pedroteosousa@gmail.com>
2018-05-03 21:55:34 -0300

    Flow setup and basic codeforces integration - Setting up flow and babel - Adding basic getContests functionality to codeforces

---
+++ src/types/contest.js
@@ -0,0 +1,14 @@
+// @flow
+export type Judge = 'codeforces' | 'csacademy';
+export type State = 'UPCOMING' | 'RUNNING' | 'FINISHED';
+
+export type Contest = {
+	name: string;
+	code: string;
+	judge: Judge;
+	startTime?: Date;
+	duration: number;
+	state?: State;
+	url: string;
+	description?: string;
+}
