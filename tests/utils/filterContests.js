b279dccdf441ec79e9e56515013d44185400335b
Pedro Sousa <pedroteosousa@gmail.com>
2018-05-06 08:13:25 -0300

    Added tests for utils/filterContests

---
+++ tests/utils/filterContests.js
@@ -0,0 +1,18 @@
+import { filterContests } from '../../src/utils';
+
+function testFilter(contests, from, to) {
+  const filteredContests = contests.filter(contest => {
+    return contest.startTime >= from && contest.startTime <= to;
+  });
+  contests = filterContests(contests, from, to);
+  return expect(contests).toMatchObject(filteredContests);
+}
+
+test('filter contests', () => {
+  const cs = [];
+  for (let i = 1; i <= 10; i++)
+    cs.push({
+      startTime: new Date(i * 1000),
+    });
+  return testFilter(cs, new Date(1000), new Date(7000));
+});
