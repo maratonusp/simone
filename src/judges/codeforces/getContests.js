568cf17c7555e5eaab368f3ac8ccc81f61256d71
Pedro Sousa <pedroteosousa@gmail.com>
2018-05-06 07:19:35 -0300

    moved contests filter to utils folder

--- src/judges/codeforces/getContests.js
+++ src/judges/codeforces/getContests.js
@@ -1,6 +1,7 @@
 // @flow
 import request from 'request-promise-native';
 import type { Contest } from '../../types';
+import { filterContests } from '../../utils';
 
 type CFContest = {
   name: string,
@@ -19,47 +20,37 @@ export async function getContests(
     await request('http://codeforces.com/api/contest.list'),
   );
 
-  let contests = cfData.result.map((contestData: CFContest) => {
-    // setting required fields
-    const contest: Contest = {
-      name: contestData.name,
-      code: contestData.id,
-      judge: 'codeforces',
-      url: 'http://codeforces.com/contests/' + contestData.id,
-    };
+  return filterContests(
+    cfData.result.map((contestData: CFContest) => {
+      // setting required fields
+      const contest: Contest = {
+        name: contestData.name,
+        code: contestData.id,
+        judge: 'codeforces',
+        url: 'http://codeforces.com/contests/' + contestData.id,
+      };
 
-    // setting fields that may be absent
-    if (contestData.startTimeSeconds != null)
-      contest.startTime = new Date(contestData.startTimeSeconds * 1000);
-    if (contestData.durationSeconds != null)
-      contest.duration = contestData.durationSeconds;
-    if (contestData.description != null)
-      contest.description = contestData.description;
+      // setting fields that may be absent
+      if (contestData.startTimeSeconds != null)
+        contest.startTime = new Date(contestData.startTimeSeconds * 1000);
+      if (contestData.durationSeconds != null)
+        contest.duration = contestData.durationSeconds;
+      if (contestData.description != null)
+        contest.description = contestData.description;
 
-    // setting contest state
-    if (contestData.phase === 'BEFORE') contest.state = 'UPCOMING';
-    else if (
-      ['CODING', 'PENDING_SYSTEM_TEST', 'SYSTEM_TEST'].includes(
-        contestData.phase,
+      // setting contest state
+      if (contestData.phase === 'BEFORE') contest.state = 'UPCOMING';
+      else if (
+        ['CODING', 'PENDING_SYSTEM_TEST', 'SYSTEM_TEST'].includes(
+          contestData.phase,
+        )
       )
-    )
-      contest.state = 'RUNNING';
-    else if (contestData.phase === 'FINISHED') contest.state = 'FINISHED';
+        contest.state = 'RUNNING';
+      else if (contestData.phase === 'FINISHED') contest.state = 'FINISHED';
 
-    return contest;
-  });
-
-  if (from) {
-    const localFrom = from;
-    contests = contests.filter(
-      contest => contest.startTime != null && contest.startTime >= localFrom,
-    );
-  }
-  if (to) {
-    const localTo = to;
-    contests = contests.filter(
-      contest => contest.startTime != null && contest.startTime <= localTo,
-    );
-  }
-  return contests;
+      return contest;
+    }),
+    from,
+    to,
+  );
 }
