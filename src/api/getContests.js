acec5d1515a0f44e5a2b5594ea57db01187de4a5
Yan Couto <yancouto@gmail.com>
2018-05-07 15:50:08 -0300

    Using await in api/getContests
    And installing necessary packages for that.

--- src/api/getContests.js
+++ src/api/getContests.js
@@ -3,21 +3,18 @@
 import type { Contest } from '../types/contest';
 import judges from '../judges';
 
-function getContests(): Promise<Array<Contest>> {
-  let ans: Array<Contest> = [];
-  let promises: Array<Promise<Array<Contest>>> = [];
+async function getContests(): Promise<Array<Contest>> {
+  const ans: Array<Contest> = [];
+  const promises: Array<Promise<Array<Contest>>> = [];
   Object.keys(judges).forEach(judge => {
     promises.push(judges[judge].getContests().catch(
       () => { console.error(judge + ' failed to getContests.'); return []; }
     ));
   });
-  return Promise.all(promises).then(
-    results => {
-      for(let contests of results)
-        ans.push(...contests);
-      return ans;
-    }
-  );
+  const results = await Promise.all(promises);
+  for(const contests of results)
+    ans.push(...contests);
+  return ans;
 }
 
 export default {
