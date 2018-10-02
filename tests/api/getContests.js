c5085efee2c3512f7504551eb20664e553b84f69
Yan Couto <yancouto@gmail.com>
2018-10-02 12:57:07 -0300

    Adding more filtering tests (each judge and api)

--- tests/api/getContests.js
+++ tests/api/getContests.js
@@ -8,7 +8,6 @@ function blocked_object() {
   return blocked;
 }
 
-// I need more than one judge working to make better tests.
 test('concatenates contest lists correctly', () => {
   // The blocked object has revoked access, that means none of its fields may be acessed.
   // This is to make sure getContests doesn't even look at any of its fields.
@@ -41,6 +40,18 @@ test('ignores if codeforces fails', () => {
 
 test('ignores if all judges fails', () => {
   for (const judge of Object.values(judges))
-    judge.getContests.mockImplementation(() => Promise.reject('error'));
+    judge.getContests = jest.fn(() => Promise.reject('error'));
   return expect(api.getContests()).resolves.toEqual([]);
+});
+
+test('filtering is correctly passed to judges', async () => {
+  const from = new Date(Date.UTC(2017, 10, 10));
+  const to = new Date(Date.UTC(2018, 10, 10));
+  Object.keys(judges).forEach(
+    judge => (judges[judge].getContests = jest.fn(() => Promise.resolve([]))),
+  );
+  expect(await api.getContests(from, to)).toEqual([]);
+  // each judge is called once with expected arguments
+  for (const judge of Object.values(judges))
+    expect(judge.getContests.mock.calls).toEqual([[from, to]]);
 });
--- tests/judges/codeforces/getContests.js
+++ tests/judges/codeforces/getContests.js
@@ -2,24 +2,16 @@ jest.mock('request');
 const request = require('request');
 import { getContests } from './../../../src/judges/codeforces/getContests';
 
+function mapToAPIReturn(...contests) {
+  return JSON.stringify({
+    status: 'OK',
+    result: contests,
+  });
+}
+
 function testContests(...contests) {
-  request.mockReturnValueOnce(
-    Promise.resolve(
-      JSON.stringify({
-        status: 'OK',
-        result: contests,
-      }),
-    ),
-  );
-  return expect(getContests()).resolves.toMatchObject(
-    contests.map(contest => ({
-      name: contest.name,
-      code: contest.id,
-      judge: 'codeforces',
-      duration: contest.durationSeconds,
-      state: 'UPCOMING',
-    })),
-  );
+  request.mockReturnValueOnce(Promise.resolve(mapToAPIReturn(...contests)));
+  return expect(getContests()).resolves.toMatchSnapshot();
 }
 
 test('simple contest', () => {
@@ -42,4 +34,27 @@ test('multiple contests', () => {
       phase: 'BEFORE',
     });
   return testContests(...cs);
+});
+
+test('filtering', () => {
+  const baseContest = {
+    name: '1',
+    id: '123',
+    durationSeconds: 111,
+    phase: 'BEFORE',
+  };
+  const inside = [
+    { ...baseContest, startTimeSeconds: 232 },
+    { ...baseContest, startTimeSeconds: 234 },
+  ];
+  const outside = [
+    { ...baseContest, startTimeSeconds: 230 },
+    { ...baseContest, startTimeSeconds: 250 },
+  ];
+  request.mockReturnValueOnce(
+    Promise.resolve(mapToAPIReturn(...inside, ...outside)),
+  );
+  return expect(
+    getContests(new Date(231000), new Date(235000)),
+  ).resolves.toMatchSnapshot();
 });
